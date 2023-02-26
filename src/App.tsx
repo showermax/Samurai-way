4fc000139005a8369be00cf7adb1a35fde00e324
showermax <sevenhauz@gmail.com>
2023-02-27 00:51:22 +0300

    Samurai-way 07 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,34 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies />
+        </div>
+    );
+}
+function Technologies() {
+    return (
+        <div className="App">
+            Hello, samurai! Let's go!
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+function Header() {
+    return (
+        <div className="App-header">
+            <a> Home </a>
+            <a> News feed </a>
+            <a> Messages </a>
+        </div>
+    );
 }
 
 export default App;
