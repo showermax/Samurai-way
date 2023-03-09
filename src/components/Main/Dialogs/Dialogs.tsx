a06966a6f45683258033b70615ec672cd2519c9f
showermax <sevenhauz@gmail.com>
2023-03-09 17:18:15 +0300

    Samurai-way map method + css

--- src/components/Main/Dialogs/Dialogs.tsx
+++ src/components/Main/Dialogs/Dialogs.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 //import "./Profile.module.css";
 import style from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
+type DialogsType = {
+    id: number
+    name: string
+}
 
 export function Dialogs() {
+    type DialogsType = {
+        id: number
+        name: string
+    }
+    const dialogsList: Array<DialogsType> = [
+        {id: 1, name: 'Danik'},
+        {id: 1, name: 'Kirill'},
+        {id: 1, name: 'Ivan'},
+        {id: 1, name: 'Denis'},
+        {id: 1, name: 'Andrei'},
+        {id: 1, name: 'Demiyan'},
+        {id: 1, name: 'Arsenyi'}
+    ]
     return (
         <div className={style.wrapper}>
-            Dialogs
+            <div className={style.dialogs}>
+                {dialogsList.map(el => <div className={style.dialog_item}><NavLink to={'/dialogs/' + el.name}
+                                                                                   activeClassName={style.activeLink}>{el.name}</NavLink>
+                </div>)}
+            </div>
+            <div className={style.messages}>
+                <div className={style.messages1}>Hi</div>
+                <div className={style.messages2}>hey</div>
+                <div className={style.messages1}>how's it going</div>
+                <div className={style.messages1}>home?</div>
+                <div className={style.messages2}>yeah, chilling .. you?</div>
+            </div>
         </div>
     );
 }
