8ba66a29893bab9be3d0c98ed26fd98943a6aa7b
showermax <sevenhauz@gmail.com>
2023-04-01 21:30:06 +0300

    navlink style

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./Navbar.module.css"
 import {NavLink} from "react-router-dom";
 
 export function Navbar() {
+    let [opacity, setOpacity]=useState (false)
     return (
         <nav className={style.nav}>
             <div className={style.item}>
-                <NavLink to='/profile' activeClassName={style.activeLink}>Profile</NavLink>
+                <NavLink to='/profile' activeClassName={style.activeLink}><div onMouseOver={()=>{setOpacity(true)}} onMouseLeave={()=>{setOpacity(false)}} className={style.nav_ico}>P</div>{opacity && <div className={style.tip}>Profile</div>}</NavLink>
             </div>
             <div className={style.item}>
                 <NavLink to='/dialogs' activeClassName={style.activeLink}>Messages</NavLink>
